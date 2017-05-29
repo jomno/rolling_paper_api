288660240343394650076cfb29ae56dc7afe0601
nojongwon <jake8955@naver.com>
2017-05-29 07:06:29 +0000

    hihi

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -6,7 +6,7 @@ class KakaoController < ApplicationController
   end
   #응답 부분
   def message
-    @answer = {message: { text: params[:content].to_s+"안녕"}, keyboard: {type: :buttons , buttons: %w(김성준 김은식 임규산)}}
+    @answer = {message: { text: params[:content].to_s+" 안녕", message_button: {label: "블로그로 가기", url: "http://wantknow.tistory.com/"}}, keyboard: {type: :buttons , buttons: %w(김성준 김은식 임규산)}}
     render json: @answer
   end
 end
